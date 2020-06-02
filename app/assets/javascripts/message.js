11af96e0ef6fec6cf881475190d3e5e6203488a3
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-03 01:40:12 +0900

    add_template_literal_message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function(){
+  function buildHTML(message) {
+    if (message.image) {
+      var html = `<div class="chat-main__main__contents">
+                    <div class="chat-main__main__contents__user">
+                      <a class="chat-main__main__contents__user__name"> 
+                        ${message.user_name}
+                      </a>
+                      <a class="chat-main__main__contents__user__date">
+                        ${message.created_at}
+                      </a>
+                    </div>
+                    <div class="chat-main__main__contents__message">
+                      <a class="chat-main__main__contents__message__content">
+                        ${message.text}
+                      </a>
+                      <img src="${message.picture}" class="chat-main__main__contents__message__image" >
+                    </div>
+                  </div>`
+    } else {
+      var html = `<div class="chat-main__main__contents">
+                    <div class="chat-main__main__contents__user">
+                      <a class="chat-main__main__contents__user__name"> 
+                        ${message.user_name}
+                      </a>
+                      <a class="chat-main__main__contents__user__date">
+                        ${message.created_at}
+                      </a>
+                    </div>
+                    <div class="chat-main__main__contents__message">
+                      <a class="chat-main__main__contents__message__content">
+                        ${message.text}
+                      </a>
+                    </div>
+                  </div>`
+    };
+    return html;
+  }
+
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     let formData = new FormData(this);
